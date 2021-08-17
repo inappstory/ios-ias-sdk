d81b5ced40a966dbae692c668dfe6a70618645c2
Pavel Eroshin <stpashik@gmail.com>
2021-08-17 12:05:35 +0300

    Update SPM Manifest

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "InAppStorySDK",
-                      url: "https://github.com/inappstory/ios-sdk/releases/download/1.7.3/InAppStorySDK-1.7.3.xcframework.zip",
-                      checksum: "b073d53eac007e5f8a23fea58939652b867b0c84612d15170c4aae89725bf44c"
+                      url: "https://github.com/inappstory/ios-sdk/releases/download/1.7.4/InAppStorySDK-1.7.4.xcframework.zip",
+                      checksum: "c7e0d209007b6ff70e0e71e5b573f4cdcf6d6dcdd8818e8b0b3299b0794325da"
                 )
     ],
     swiftLanguageVersions: [.v5]
