c485265d8cb02d7b044eb9ba5986ee52d8e7b92a
Pavel Eroshin <stpashik@gmail.com>
2022-07-20 14:43:32 +0300

    add images placeholders; add `payload` in notification "ShowSlide" stability improvements;

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "InAppStorySDK_SwiftUI",
-                      url: "https://cs.inappstory.com/iOS/InAppStorySDK_SwiftUI-1.16.1.xcframework.zip",
-                      checksum: "68d64073310caf9baeefb82855aa356f1b117059559b9a8732c9df8f2cedfdb8"
+                      url: "https://cs.inappstory.com/iOS/InAppStorySDK_SwiftUI-1.17.0.xcframework.zip",
+                      checksum: "3d16fa509238b39ed3ca707afa63a217aeefe9878359b17667eecfdf17cef2d4"
                 )
     ],
     swiftLanguageVersions: [.v5]
