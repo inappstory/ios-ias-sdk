df5697a61f9dcaa10ebf068725a78eb6e2bde3bc
StPashik <stpashik@gmail.com>
2023-01-19 11:43:14 +0300

    fixes; stability improvements;

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "InAppStorySDK",
-                      url: "https://cs.inappstory.com/iOS/InAppStorySDK-1.19.3.xcframework.zip",
-                      checksum: "883b089deb925a3897ec96e1273b4d49296e6a094e0ce1c2fc2bcde319d9987c"
+                      url: "https://cs.inappstory.com/iOS/InAppStorySDK-1.19.4.xcframework.zip",
+                      checksum: "9c61bb2ec878f9496f995585b5cfceaeaa1e7dd39c235cb50d4cca6e3ffe14de"
                 )
     ],
     swiftLanguageVersions: [.v5]
