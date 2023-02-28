1119a4c30e446cc3725792df1c0c479c91de2693
Pavel Eroshin <stpashik@gmail.com>
2023-02-28 19:31:17 +0300

    stability improvements;

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "InAppStorySDK_SwiftUI",
-                      url: "https://cs.inappstory.com/iOS/InAppStorySDK_SwiftUI-1.20.3.xcframework.zip",
-                      checksum: "8041b108aa1d64cc896832d1afc0a13fca9fae76f62b5475678f534282f2ba3d"
+                      url: "https://cs.inappstory.com/iOS/InAppStorySDK_SwiftUI-1.20.4.xcframework.zip",
+                      checksum: "ce56f00619d94793370ade69ce1c7ab0c577ae1aabf455fc86654723eda3573a"
                 )
     ],
     swiftLanguageVersions: [.v5]
