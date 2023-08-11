0894f25b96c8899209e37fc87f8531158af70d81
StPashik <pavel.eroshin@inappstory.ru>
2023-08-11 08:01:42 -0700

    CI build 28 for InAppStoryUGC 1.2.3 (412)

---
+++ InAppStoryUGC.xcframework/ios-arm64/InAppStoryUGC.framework/Headers/InAppStoryUGC.h
@@ -0,0 +1,18 @@
+//
+//  InAppStoryUGC.h
+//  InAppStoryUGC
+//
+//  Created by StPashik on 08.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InAppStoryUGC.
+FOUNDATION_EXPORT double InAppStoryUGCVersionNumber;
+
+//! Project version string for InAppStoryUGC.
+FOUNDATION_EXPORT const unsigned char InAppStoryUGCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppStoryUGC/PublicHeader.h>
+
+
